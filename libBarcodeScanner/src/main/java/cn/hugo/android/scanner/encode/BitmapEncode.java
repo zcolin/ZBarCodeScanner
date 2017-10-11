03ce7778750713ea679d0b0a688b9a6d05477a6c
colin <wanglin2046@126.com>
2017-10-11 16:22:53 +0800

    # 可以设置二维码颜色

--- libBarcodeScanner/src/main/java/cn/hugo/android/scanner/encode/BitmapEncode.java
+++ libBarcodeScanner/src/main/java/cn/hugo/android/scanner/encode/BitmapEncode.java
@@ -16,10 +16,19 @@ import java.util.Hashtable;
 
 public class BitmapEncode {
     /**
-     * 生成二维码图片，在编码时需要将com.google.zxing.qrcode.encoder.Encoder.java中的
-     * static final String DEFAULT_BYTE_MODE_ENCODING = "ISO8859-1";修改为UTF-8，否则中文编译后解析不了
+
      */
     public static Bitmap encode(String content, int width, int height) {
+        return encode(content, width, height, -1);
+    }
+
+    /**
+     * 生成二维码图片，在编码时需要将com.google.zxing.qrcode.encoder.Encoder.java中的
+     * static final String DEFAULT_BYTE_MODE_ENCODING = "ISO8859-1";修改为UTF-8，否则中文编译后解析不了
+     *
+     * @param color 二维码颜色
+     */
+    public static Bitmap encode(String content, int width, int height, int color) {
         Bitmap bitmap = null;
         try {
             MultiFormatWriter writer = new MultiFormatWriter();
@@ -28,10 +37,11 @@ public class BitmapEncode {
             hst.put(EncodeHintType.CHARACTER_SET, "UTF-8");
             BitMatrix matrix = writer.encode(content, BarcodeFormat.QR_CODE, width, height, hst);
             int[] pixels = new int[width * height];
+            color = color >= 0 ? color : 0xff000000;
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     if (matrix.get(x, y)) {
-                        pixels[y * width + x] = 0xff000000;
+                        pixels[y * width + x] = color;
                     } else
                     //无信息设置像素点为白色 不设置会导致保存图片的时候一团黑
                     {
@@ -50,15 +60,24 @@ public class BitmapEncode {
      * 带有logo的二维码图片
      */
     public static Bitmap encodeWithPic(Context context, int drawable, String content, int width, int height) {
+        return encodeWithPic(context, drawable, content, width, height, -1);
+    }
+
+    public static Bitmap encodeWithPic(Context context, int drawable, String content, int width, int height, int color) {
         Bitmap bitmapPic = BitmapFactory.decodeResource(context.getResources(), drawable);
-        return encodeWithPic(bitmapPic, content, width, height);
+        return encodeWithPic(bitmapPic, content, width, height, color);
+    }
+
+
+    public static Bitmap encodeWithPic(Bitmap bitmapPic, String content, int width, int height) {
+        return encodeWithPic(bitmapPic, content, width, height, -1);
     }
 
     /**
      * 带有logo的二维码图片
      */
-    public static Bitmap encodeWithPic(Bitmap bitmapPic, String content, int width, int height) {
-        Bitmap bitmap = encode(content, width, height);
+    public static Bitmap encodeWithPic(Bitmap bitmapPic, String content, int width, int height, int color) {
+        Bitmap bitmap = encode(content, width, height, color);
         int leftLogo = bitmap.getWidth() / 2 - bitmapPic.getWidth() / 2;
         int topLogo = bitmap.getHeight() / 2 - bitmapPic.getHeight() / 2;
         Paint paint = new Paint();
